17b37002fe32014e739abbb51dc413848cb7cf6c
Silvio <silvio@pop-os.localdomain>
2023-04-17 11:57:22 +0200

    minor bug update

--- build/lib/stockpy/neural_network/__init__.py
+++
@@ -1,5 +0,0 @@
-from ._base import LSTM
-from ._base import GRU
-from ._base import BiLSTM
-from ._base import BiGRU
-from ._base import MLP
--- build/lib/stockpy/probabilistic/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._base import BayesianNN
-from ._base import GaussianHMM
-from ._base import DeepMarkovModel
